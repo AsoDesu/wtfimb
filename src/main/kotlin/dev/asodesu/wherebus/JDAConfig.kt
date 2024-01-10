8368decd9c859b59a5028d79b6e51b78b9d58e02
Aso <53526710+AsoDesu@users.noreply.github.com>
2024-01-10 19:31:20 +0000

    feat: add some pre-init stuff and make everything a bit more user-friendly

--- src/main/kotlin/dev/asodesu/wherebus/JDAConfig.kt
+++ src/main/kotlin/dev/asodesu/wherebus/JDAConfig.kt
@@ -5,13 +5,18 @@ import dev.asodesu.wherebus.commands.CommandHandler
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.hooks.AnnotatedEventManager
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import kotlin.math.log
+import kotlin.system.exitProcess
 
 @Configuration
 class JDAConfig {
+    val logger = LoggerFactory.getLogger(this::class.java)
+
     @Value("\${discord.bot-token}")
     lateinit var token: String
 
@@ -19,6 +24,14 @@ class JDAConfig {
     lateinit var commands: List<Command>
     @Bean
     fun jda(): JDA {
+        if (token.isBlank()) {
+            logger.error("")
+            logger.error("Discord Bot Token has not been set!")
+            logger.error("You must set the 'discord.bot-token' config value to a bot token from the " +
+                    "Discord Developer Portal (https://discord.com/developers/applications)")
+            exitProcess(0)
+        }
+
         val jda = JDABuilder.createDefault(token)
             .setEventManager(AnnotatedEventManager())
             .build()
--- src/main/kotlin/dev/asodesu/wherebus/WherebusApplication.kt
+++ src/main/kotlin/dev/asodesu/wherebus/WherebusApplication.kt
@@ -1,11 +1,52 @@
 package dev.asodesu.wherebus
 
+import org.apache.tomcat.util.http.fileupload.FileUtils
+import org.slf4j.LoggerFactory
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.core.io.ClassPathResource
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.util.regex.Pattern
+import kotlin.math.log
 
 @SpringBootApplication
 class WherebusApplication
 
-fun main(args: Array<String>) {
-    runApplication<WherebusApplication>(*args)
+object WherebusBootstrap {
+    val logger = LoggerFactory.getLogger("StartBootstrap")
+    val serverPortRegex = Pattern.compile("(server\\.port=)(\\d+)")
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val propertiesFile = File("./application.properties")
+        if (!propertiesFile.exists()) {
+            val resource = ClassPathResource("default.properties")
+            val stream = resource.url.openStream()
+            if (stream == null) {
+                logger.error("Uh oh.. couldn't find default properties resource!")
+                return
+            } else {
+                Files.copy(stream, propertiesFile.toPath())
+                stream.close()
+            }
+
+            logger.error("Properties file was not found, so the default one has been created!")
+            logger.error("Please configure the bot in the 'app.properties' file and start up again!")
+            return
+        }
+        val app = SpringApplication(WherebusApplication::class.java)
+
+        val content = propertiesFile.readText()
+        val hasServerPort = serverPortRegex.matcher(content).find()
+        if (!hasServerPort) {
+            logger.info("Server Port has not been set, Web App disabled!")
+            app.webApplicationType = WebApplicationType.NONE
+        }
+
+        app.run(*args)
+    }
 }
